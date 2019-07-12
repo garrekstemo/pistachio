de6729d9a2978ba8b7c281614b365563c167866c
Garrek Stemo <garrekstemo@icloud.com>
2019-07-12 17:23:39 +0900

    TMM now implements a layer as an object.

--- transfer_matrix_method.py
+++ transfer_matrix_method.py
@@ -13,136 +13,123 @@ import TMM_tests as TMM
 c = sc.c  # speed of light
 h = sc.h  # planck's constant
 
+
 class Light:
 
-	def __init__(self, wavelength_):
+	def __init__(self, wavelength):
 		
-		self.wavelength_ = wavelength_
-		self.omega = 2*np.pi*c / wavelength_  # angular frequency
-		self.freq = c / wavelength_  # frequency
-		self.k = 2*np.pi / wavelength_  # wavenumber
-		self.energy_J = h*c / wavelength_  # energy in Joules
+		self.wavelength_ = wavelength
+		self.omega = 2*np.pi*c / wavelength  # angular frequency
+		self.freq = c / wavelength  # frequency
+		self.k = 2*np.pi / wavelength  # wavenumber
+		self.energy_J = h*c / wavelength  # energy in Joules
 		self.energy_ev = self.energy_J / sc.eV  # energy in electronvolts
 
-def wavenumber(n, omega, theta=0):
+
+class Layer:
+
+	def __init__(self, material, index, thickness):
+		self.material = material
+		self.index = index
+		self.thickness = thickness
+
+	def wavenumber(self, omega, theta=0):
+		"""Outputs the wavenumbers for the dielectric for the given
+		   angular frequency and angle"""
+		k_x = self.index*omega/sc.c * np.cos(theta)
+		k_z = self.index*omega/sc.c * np.sin(theta)
+		return k_x, k_z
+
+	def propagation_matrix(self, wavenumber):
+		"""Inputs: wave number, thickness of medium
+		   Output: propagation matrix (phase accumulation for plane wave 
+					propagating through homogeneous medium)."""	
+		phi = wavenumber*self.thickness
+		P_i = np.matrix([[np.exp(-1j*phi), 0], [0, np.exp(1j*phi)]])
 	
-	k_x = n*omega/c * np.cos(theta)
-	k_z = n*omega/c * np.sin(theta)
-	return k_x, k_z
+		return P_i
 
-
-def propagation_matrix(k_, d_):
-	"""Inputs: wave number, thickness of medium
-	   Output: propagation matrix (phase accumulation for plane wave 
-	   			propagating through homogeneous medium)."""	
-	phi = k_*d_
-	i = 1j
-	P_i = np.matrix([[np.exp(-i*phi), 0], [0, np.exp(i*phi)]])
-	
-	return P_i
-
-
-def dynamical_matrix(n, theta=0):
-	"""Inputs: index of refraction, angle of incidence
-		Outputs: dynamical matrices for s-wave and p-wave."""
-
-	# s-wave dynamical matrix
-	m = n * np.cos(theta)
-	Ds = np.matrix([[1, 1], [m, -m]])
+	def dynamical_matrix(self, theta=0.):
+		"""Inputs: index of refraction, angle of incidence
+			Outputs: dynamical matrices for s-wave and p-wave."""
+		n = self.index
+		
+		# s-wave dynamical matrix
+		m = n * np.cos(theta)
+		Ds = np.matrix([[1, 1], [m, -m]])
 					 
-	# s-wave dynamical matrix
-	Dp = np.matrix([[np.cos(theta), np.cos(theta)], [n, -n]])
-		 
-	return Ds, Dp
+		# s-wave dynamical matrix
+		Dp = np.matrix([[np.cos(theta), np.cos(theta)], [n, -n]])
+		
+		return Ds, Dp
 
-
-def make_layer(n, k_x, d, theta=0):
-
-	D = dynamical_matrix(n, theta)[0]
-	Dinv = np.linalg.inv(D)
-	P = propagation_matrix(k_x, d)
-	layer = np.linalg.multi_dot([D, P, Dinv])
+	def structure(n, k_x, d, theta=0):
+		"""Maybe deprecate this"""
+		k_x = self.wavenumber()[0]
+		D = dynamical_matrix(self.index, self.theta)[0]
+		Dinv = np.linalg.inv(D)
+		P = propagation_matrix(k_x, self.thickness)
+		layer = np.linalg.multi_dot([D, P, Dinv])
 	
-	return layer
-	
+		return layer
+		
+		
+def multilayer_matrix(array):
+	TM = np.linalg.multi_dot(array)
+	return TM
+
+def inverse(M):
+	Minv = np.linalg.inv(M)
+	return Minv
+
+def reflectance(M_):
+    """Input: multilayer matrix, M.
+       Output: reflectance calculation."""
+    M21 = M_.item((1, 0))
+    M11 = M_.item((0, 0))
+    r = M21 / M11
+    r_sq = r * np.conj(r)
+    r_sq = r_sq.real
+    R = r_sq
+   
+    return R, r
+   
+def transmittance(M_, n0_, ns_, theta_0=0, theta_s=0):
+    """Inputs: Multilayer matrix of dynamical matrices and propagation matrix."""
+    M11 = M_.item((0, 0))
+    t = 1/M11
+    t_sq = t * np.conj(t)
+    t_sq = t_sq.real
+
+    #T = ((ns_*np.cos(theta_s)) / (n0_*np.cos(theta_0))) * t_sq
+    T = np.linalg.det(M_) * t_sq
+
+    return T, t
+
 
 def fresnel(n1, n2, k1x, k2x):
 	"""Inputs: angular frequency of incident light
 			   refractive indices for two media
 			   angle of incidence (to calculate wavenumbers
 	   Outputs: reflection and transmission coefficients for s and p waves."""
-	
+
 	# s-wave reflection, transmission coefficients
 	r12_s = (k1x - k2x) / (k1x + k2x)
 	t12_s = 2*k1x / (k1x + k2x)
-	
+
 	# p-wave reflection, transmission coefficients
 	r12_p = (n1*n1*k2x - n2*n2*k1x) / (n1*n1*k2x + n2*n2*k1x)
 	t12_p = (2*n1*n1*k2x) / (n1*n1*k2x + n2*n2*k1x)
-	
+
 	return r12_s, t12_s, r12_p, t12_p
 
 
 def transmission_matrix(r_ij, t_ij):
-	"""I don't think I'm actually using this yet. Delete comment when used."""
 	"""Inputs: Fresnel transmission, reflection coefficients from medium i to medium j.
 	   Output: Corresponding transmission matrix linking amplitudes
 	   from medium i to medium j."""
 	D_ij = 1/t_ij * np.matrix([[1, r_ij], [r_ij, 1]])    
 	return D_ij
-	
-
-def multilayer_matrix(n0, n1, ns, d, omega, theta=0, num_layers=0):
-
-	TOL = 1e-06  #small number tolerance
-
-	k1x = wavenumber(n1, omega, theta)[0]
-
-
-	D0 = dynamical_matrix(n0, theta)[0] # Incident medium
-	D0inv = np.linalg.inv(D0)
-	D1 = dynamical_matrix(n1, theta)[0]
-	D1inv = np.linalg.inv(D1)
-	Ds = dynamical_matrix(ns, theta)[0] # Transmit medium
-	
-	L = make_layer(n1, k1x, d)
-	
-	M = np.linalg.multi_dot([D0inv, L, Ds])
-
-	#M.real[abs(M.real) < TOL] = 0.0
-	#M.imag[abs(M.imag) < TOL] = 0.0
-	
-	# Quarter-wave stack test
-# 	M = D0inv*P0*D0 * D1*P1*D1inv  # Quarter-wave stack	
-	#M_quarter = np.matrix([[-n1/n0, 0], [0, -n0/n1]])
-	
-	return M
-
-
-
-def reflectance(M_):
-	"""Input: multilayer matrix, M.
-	   Output: reflectance calculation."""
-	M21 = M_.item((1, 0))
-	M11 = M_.item((0, 0))
-	r = M21 / M11
-	r_sq = r * np.conj(r)
-	r_sq = r_sq.real
-	R = r_sq
-	   
-	return R, r
-	   
-def transmittance(M_, n0_, ns_, theta_0=0, theta_s=0):
-	"""Inputs: Multilayer matrix of dynamical matrices and propagation matrix."""
-	M11 = M_.item((0, 0))
-	t = 1/M11
-	t_sq = t * np.conj(t)
-	t_sq = t_sq.real
-	
-	#T = ((ns_*np.cos(theta_s)) / (n0_*np.cos(theta_0))) * t_sq
-	T = np.linalg.det(M_) * t_sq
-	
-	return T, t
 
 
 def make_wavelengths(num_points, min_l, max_l):
@@ -163,7 +150,6 @@ def make_wavelengths(num_points, min_l, max_l):
 	
 
 def get_wave_data(data_file):
-	"""Not Implemented"""
 	with open(data_file, 'r') as f:
 		wavelength = []
 		y = []
@@ -182,8 +168,6 @@ def get_wave_data(data_file):
 	return wavelength, y, K
 
 
-
-
 def main():
 	
 	# Inputs
@@ -200,7 +184,7 @@ def main():
 
 	# Get a bunch of downloaded data
 	wavelen_n, Au_n, Au_K = get_wave_data(data_Au_n)
-	wavelen_T = get_wave_data(data_Au_T)[0]  # experimental data for Au T
+	wavelen_T = get_wave_data(data_Au_T)[0]  # experimental data for Au transmission
 	Au_T = get_wave_data(data_Au_T)[1]
 	
 	# Outputs
@@ -210,20 +194,31 @@ def main():
 	NUM_POINTS = len(Au_n)
 	i = 0
 	while i < NUM_POINTS:
-# 		print("="*60)
+
 		lmbda = wavelen_n[i] * 10**-6
-		n_au = Au_n[i] + 1j*Au_K[i]
+		n = Au_n[i] + 1j*Au_K[i]
 		light = Light(lmbda)
 		omega = light.omega
+		air = Layer('air', 1.0, 0.)
+		au = Layer('gold', n, d)
+		other = Layer('other', n_other, 0.)
 
 		#Get wavenumbers for two media
-		k0x = wavenumber(n_air, omega)[0]
-		k1x = wavenumber(n_au, omega)[0]
+		k0x = air.wavenumber(omega)[0]
+		k1x = au.wavenumber(omega)[0]
 		
 		#Use dynamical matrix
-		M_i = multilayer_matrix(n_air, n_au, n_other, d, omega)  
+		D0 = air.dynamical_matrix()[0]
+		D0inv = inverse(D0)
+		D1 = au.dynamical_matrix()[0]
+		D1inv = inverse(D1)
+		P1 = au.propagation_matrix(k1x)
+		D2 = other.dynamical_matrix()[0]
+		
+		M_list = [D0inv, D1, P1, D1inv, D2]
+		M_i = multilayer_matrix(M_list)
 
-		trans = transmittance(M_i, n_air, n_other)[0].real
+		trans = transmittance(M_i, air.index, other.index)[0].real
 		refl = reflectance(M_i)[0]
 		T.append(trans)
 		R.append(refl)
@@ -256,7 +251,7 @@ def main():
 	ax.legend()
 	
 	fig.tight_layout()
-	plt.show()
+# 	plt.show()
 	
 
 if __name__ == '__main__':
