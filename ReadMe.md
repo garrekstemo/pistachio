6c2ab0c9f4fe49673e252bec4b9bb8981b3d5699
Garrek Stemo <garrekstemo@icloud.com>
2019-07-16 19:58:25 +0900

    Implemented object-oriented and yaml config file.

--- ReadMe.md
+++ ReadMe.md
@@ -12,3 +12,25 @@ layers while still being easy to read and configure.
 ## Data directory
 
 Data in the data directory are downloaded from refractiveindex.info unless stated otherwise.
+
+## Config files
+
+Config files are stored in the .yaml format as a device with a specified
+number of layers and configuration for each layer. For example, a device might
+look like the following
+
+num_layers: 2
+layers:
+    layer0:
+        material: SiO2 
+        thickness: 1000 
+        param_path: "/Users/project/params/layer0.csv"
+    layer1:
+        material: Au
+        thickness 35
+        param_path: "/Users/project/params/layer1.csv"
+
+Note that thickness must be given in nanometers. The param_path specifies
+the path where refractiveindex.info data is stored, which must be saved as
+a .csv file with wavelength, refractive index, and extinction coefficient
+columns.
--- transfer_matrix_method.py
+++ transfer_matrix_method.py
@@ -4,14 +4,17 @@
 # Psi(x, t) = Psi_0 * exp(i(kx - wt))
 # n = n' + i*n'', where n' is real part of refractive index and n'' is imaginary part.
 
+import argparse
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.constants as sc
 import TMM_tests as TMM
+from ruamel_yaml import YAML
 
 c = sc.c  # speed of light
 h = sc.h  # planck's constant
+yaml = YAML()
 
 
 class Light:
@@ -23,21 +26,45 @@ class Light:
 		self.freq = c / wavelength  # frequency
 		self.k = 2*np.pi / wavelength  # wavenumber
 		self.energy_J = h*c / wavelength  # energy in Joules
-		self.energy_ev = self.energy_J / sc.eV  # energy in electronvolts
-
+		self.energy_ev = self.energy_J / sc.eV  # energy in electron-volts
+		
+# 	def omega(self):
+# 		omega = 2*np.pi*c / self.wavelength
+# 		return omega
+		
 
 class Layer:
 
-	def __init__(self, material, index, thickness):
+	def __init__(self, material, thickness=0):
 		self.material = material
-		self.index = index
 		self.thickness = thickness
+		self.wavelength = []  # array of free space wavelengths
+		self.index = []  # array of refractive indices
+		self.extinct = []  # array of extinction coefficients
+		self.complex = complex
 
-	def wavenumber(self, omega, theta=0):
+	def complex_index(self, n, K):
+		self.complex = n + 1j*K
+		
+	def get_data_from_csv(self, path):
+
+		with open(path, 'r') as params:
+			reader = csv.reader(params)
+			next(reader, None)
+			for row in reader:
+				wl = float(row[0])
+				n = float(row[1])
+				K = float(row[2])
+				self.wavelength.append(wl)
+				self.index.append(n)
+				self.extinct.append(K)
+
+	def wavenumber(self, n, omega, theta=0):
 		"""Outputs the wavenumbers for the dielectric for the given
 		   angular frequency and angle"""
-		k_x = self.index*omega/sc.c * np.cos(theta)
-		k_z = self.index*omega/sc.c * np.sin(theta)
+		k_x = n*omega/sc.c * np.cos(theta)
+		k_z = n*omega/sc.c * np.sin(theta)
+# 		print(k_x)
 		return k_x, k_z
 
 	def propagation_matrix(self, wavenumber):
@@ -49,37 +76,82 @@ class Layer:
 	
 		return P_i
 
-	def dynamical_matrix(self, theta=0.):
+	def dynamical_matrix(self, n_, theta=0.):
 		"""Inputs: index of refraction, angle of incidence
 			Outputs: dynamical matrices for s-wave and p-wave."""
-		n = self.index
 		
 		# s-wave dynamical matrix
-		m = n * np.cos(theta)
+		m = n_ * np.cos(theta)
+
 		Ds = np.matrix([[1, 1], [m, -m]])
 					 
 		# s-wave dynamical matrix
-		Dp = np.matrix([[np.cos(theta), np.cos(theta)], [n, -n]])
+		Dp = np.matrix([[np.cos(theta), np.cos(theta)], [n_, -n_]])
 		
 		return Ds, Dp
 
-	def structure(n, k_x, d, theta=0):
-		"""Maybe deprecate this"""
-		k_x = self.wavenumber()[0]
-		D = dynamical_matrix(self.index, self.theta)[0]
-		Dinv = np.linalg.inv(D)
-		P = propagation_matrix(k_x, self.thickness)
-		layer = np.linalg.multi_dot([D, P, Dinv])
+
+def get_dict_from_yaml(yaml_file):
+	"""Get data from yaml config file and put into dictionary"""
+
+	with open(yaml_file, 'r') as yml:
+		device = yaml.load(yml)
+		num_layers = int(device['num_layers'])
+		len_layers = int(len(device['layers']))
+    
+		assert_message = "num_layers ({}) and the number of layers specified ({}) in \
+                          device config file do not match.".format(num_layers, len_layers)
+		assert num_layers == len_layers, assert_message
+	return device
+    
+def get_layers_from_yaml(device_dict):
+	"""Takes device dictionary and outputs all layers as a list"""
+	num_key = 'num_layers'
+	num_layers = int(device_dict[num_key])
+	layers = []
 	
-		return layer
+	for i in range(num_layers):
+	
+		name = 'layer' + str(i)
+		layer = device_dict['layers'][name]
+		material = layer['material']
+		thickness = float(layer['thickness']) * 10**-9
+		layer_class = Layer(material, thickness)
 		
-		
+		if "param_path" in layer:
+			params = layer['param_path']
+			layer_class.get_data_from_csv(params)
+		elif "index" in layer:
+			layer_class.index = layer['index']
+			layer_class.extinct = layer['extinction']
+		else:
+			print("Error in the yaml file.")
+			
+		layers.append(layer_class)
+
+	return layers
+	
+	
+def check_data_compatibility(layer1, layer2):
+	"""NOT IMPLEMENTED.
+	   Takes a list of layers obtained from get_layers_from_yaml"""
+	
+	row_error = "rows not all the same length in {}".format(l.material)
+	column_error = "columns not all the same length in {} and {}".format(layer1.material,
+																		 layer2.material)
+	
+	assert len(layer1.wavelength) == len(layer2.wavelength), column_error
+	
+	return 0
+
 def multilayer_matrix(array):
+	"""Generates the transfer matrix for a 1D array of individual
+	   layer matrices"""
 	TM = np.linalg.multi_dot(array)
 	return TM
 
-def inverse(M):
-	Minv = np.linalg.inv(M)
+def inverse(matrix):
+	Minv = np.linalg.inv(matrix)
 	return Minv
 
 def reflectance(M_):
@@ -94,24 +166,26 @@ def reflectance(M_):
    
     return R, r
    
-def transmittance(M_, n0_, ns_, theta_0=0, theta_s=0):
+def transmittance(TM, n0=0, ns=0, theta_0=0, theta_s=0):
     """Inputs: Multilayer matrix of dynamical matrices and propagation matrix."""
-    M11 = M_.item((0, 0))
+    M11 = TM.item((0, 0))
     t = 1/M11
     t_sq = t * np.conj(t)
     t_sq = t_sq.real
 
-    #T = ((ns_*np.cos(theta_s)) / (n0_*np.cos(theta_0))) * t_sq
-    T = np.linalg.det(M_) * t_sq
+#     T = ((ns*np.cos(theta_s)) / (n0*np.cos(theta_0))) * t_sq
+    T = np.linalg.det(TM) * t_sq
 
     return T, t
 
 
+# ===== Functions below not used so much for now ==== #
+
 def fresnel(n1, n2, k1x, k2x):
-	"""Inputs: angular frequency of incident light
-			   refractive indices for two media
-			   angle of incidence (to calculate wavenumbers
-	   Outputs: reflection and transmission coefficients for s and p waves."""
+	"""Inputs:  Angular frequency of incident light
+			    refractive indices for two media
+			    angle of incidence (to calculate wavenumbers
+	   Outputs: Reflection and transmission coefficients for s and p waves."""
 
 	# s-wave reflection, transmission coefficients
 	r12_s = (k1x - k2x) / (k1x + k2x)
@@ -123,7 +197,6 @@ def fresnel(n1, n2, k1x, k2x):
 
 	return r12_s, t12_s, r12_p, t12_p
 
-
 def transmission_matrix(r_ij, t_ij):
 	"""Inputs: Fresnel transmission, reflection coefficients from medium i to medium j.
 	   Output: Corresponding transmission matrix linking amplitudes
@@ -133,23 +206,23 @@ def transmission_matrix(r_ij, t_ij):
 
 
 def make_wavelengths(num_points, min_l, max_l):
-	"""Make wavelength data from desired number of points, 
+	"""NOT IMPLEMENTED
+	   Make wavelength data from desired number of points, 
 		starting and ending wavelengths."""
 	wavelen_list = []
 	wavelen = min_l
 	data_width = (max_l - min_l) / num_points
-	
 	i = 0	
 	while i < num_points:
 		wavelen_list.append(wavelen)
 		wavelen += data_width
 		i+=1
 	wavelen_list.sort()
-		 
+ 
 	return wavelen_list
 	
-
 def get_wave_data(data_file):
+
 	with open(data_file, 'r') as f:
 		wavelength = []
 		y = []
@@ -166,79 +239,110 @@ def get_wave_data(data_file):
 	f.close()
 
 	return wavelength, y, K
-
+	
 
 def main():
+
+	parser = argparse.ArgumentParser()
+	device_help = "an argument with device.yaml file in pfiles with list of layer csv files"
+	parser.add_argument("device", help=device_help)
+	args = parser.parse_args()
+	
+# 	data_Au_n = '/Users/garrek/projects/pistachio/data/Au_SiO2_n_Ciesielski.csv'
+	data_Au_T = '/Users/garrek/projects/pistachio/data/transmittance_au_SiO2_Ciesielski.csv'
+
+	# Get a bunch of downloaded data
+# 	wavelen_n, Au_n, Au_K = get_wave_data(data_Au_n)
+	wavelen_T = get_wave_data(data_Au_T)[0]  # experimental data for Au transmission
+	Au_T = get_wave_data(data_Au_T)[1]
+
 	
 	# Inputs
 	#NUM_PTS = 100
 	#MIN = 200  #nm
 	#MAX = 20000  #nm
-	n_air = 1.0
-	n_other = 1.4
-	d = 35*10**-9  # thickness of medium
-	inc_ang = 0.
-
-	data_Au_n = '/Users/garrek/projects/pistachio/data/Au_SiO2_n_Ciesielski.csv'
-	data_Au_T = '/Users/garrek/projects/pistachio/data/transmittance_au_SiO2_Ciesielski.csv'
-
-	# Get a bunch of downloaded data
-	wavelen_n, Au_n, Au_K = get_wave_data(data_Au_n)
-	wavelen_T = get_wave_data(data_Au_T)[0]  # experimental data for Au transmission
-	Au_T = get_wave_data(data_Au_T)[1]
+	n_air = 1.0     # For testing purposes
+	n_other = 1.4   # For testing purposes
+	inc_ang = 0.    # If zero, p-wave and s-wave should yield same transmission
+	um = 10**-6     # micrometers
 	
+	device = get_dict_from_yaml(args.device)   # yaml config file stored as dictionary
+	layers = get_layers_from_yaml(device)  # a list of layer objects
+
+	air = Layer('air')
+	air.index = 1.0
+	air.extinct = 0.
+	air.complex = 1.0
+	
+	other = Layer('other')
+	other.index = 1.4
+	other.extinct = 0.
+	other.complex = 1.4
+	
+	au = layers[0]
+	Au_n = au.index
+	Au_K = au.extinct
+
 	# Outputs
-	R = []  # my reflectance data
-	T = []  # my transmittance data
-	
-	NUM_POINTS = len(Au_n)
-	i = 0
-	while i < NUM_POINTS:
+	wavelen = [i for i in layers[0].wavelength]
+	print("num of wavelengths", len(wavelen))
+	R = []  # calculated reflectance data
+	T = []  # calculated transmittance data
 
-		lmbda = wavelen_n[i] * 10**-6
-		n = Au_n[i] + 1j*Au_K[i]
-		light = Light(lmbda)
+	num_points = len(layers[0].wavelength)   # TODO: This is really sketchy.
+	num_layers = len(layers)
+
+	for i in range(num_points):
+		#TODO: Deal with data that have a different number of points 
+		M_list = []  # list of matrices to be multiplied
+		lmbda = wavelen[i] * um
+		light = Light(lmbda)  # Make an instance of Light class
 		omega = light.omega
-		air = Layer('air', 1.0, 0.)
-		au = Layer('gold', n, d)
-		other = Layer('other', n_other, 0.)
 
-		#Get wavenumbers for two media
-		k0x = air.wavenumber(omega)[0]
-		k1x = au.wavenumber(omega)[0]
-		
-		#Use dynamical matrix
-		D0 = air.dynamical_matrix()[0]
+		# Add air to matrix list (reflection region)		
+		air.wavelength = lmbda
+		D0 = air.dynamical_matrix(air.index)[0]
 		D0inv = inverse(D0)
-		D1 = au.dynamical_matrix()[0]
-		D1inv = inverse(D1)
-		P1 = au.propagation_matrix(k1x)
-		D2 = other.dynamical_matrix()[0]
+		M_list.append(D0inv)
+
+		for layer in layers:
+			#Cycle through layer object instances stored in the 'layers' list above.
+			
+			n = layer.index[i] + 1j*layer.extinct[i]
+			kx = layer.wavenumber(n, omega)[0]
+			
+			D = layer.dynamical_matrix(n)[0]
+			Dinv = inverse(D)
+			P = layer.propagation_matrix(kx)
+					
+			M_list.extend([D, P, Dinv])
+
+		# Add other to matrix list (transmission region)
+		other.wavelength = lmbda
+		Ds = other.dynamical_matrix(other.index)[0]
+		M_list.append(Ds)
+
+# 		M_i = multilayer_matrix(M_list)
+		M_i = multilayer_matrix([D0inv, D, P, Dinv, Ds])
+					
+		trn = transmittance(M_i, air.index, other.index)[0].real
+		ref = reflectance(M_i)[0]
 		
-		M_list = [D0inv, D1, P1, D1inv, D2]
-		M_i = multilayer_matrix(M_list)
-
-		trans = transmittance(M_i, air.index, other.index)[0].real
-		refl = reflectance(M_i)[0]
-		T.append(trans)
-		R.append(refl)
-
-		i+=1
+		T.append(trn)
+		R.append(ref)
+		
 	
-# 	TEST = TMM.conservation_tests(M_i, n_air, n_other)
-# 	Quarter = TMM.testing_quarterwave()
-	
-	#Make Plots
+	# Make Plots
 	fig, axs = plt.subplots(3, 1, sharex=True)
 	
 	ax = axs[0]
-	ax.plot(wavelen_n, Au_n, label="n, downloaded data")
-	ax.plot(wavelen_n, Au_K, label="K, downloaded data")
+	ax.plot(wavelen, Au_n, label="n, downloaded data")
+	ax.plot(wavelen, Au_K, label="K, downloaded data")
 	ax.set_ylabel('refractive index for Au')
 	ax.legend()
 	
 	ax = axs[1]
-	ax.plot(wavelen_T, T, label="calculated data")
+	ax.plot(wavelen, T, label="calculated data")
 	ax.plot(wavelen_T, Au_T, label="downloaded data")
 	ax.set_ylabel('transmittance')
 # 	ax.set_xlim(0, 5)
@@ -250,8 +354,9 @@ def main():
 	ax.set_ylabel('reflectance')
 	ax.legend()
 	
+	fig.suptitle('Index of refraction, transmission, reflection for {}'.format(au.material))
 	fig.tight_layout()
-# 	plt.show()
+	plt.show()
 	
 
 if __name__ == '__main__':
