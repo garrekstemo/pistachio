88ba0e4223e97228d75af60b224664bba7f94440
garrekds <garrekstemo@icloud.com>
2020-07-07 14:37:25 +0900

    Moved wavelength calculations from Layer to new Wave class

--- transfer_matrix.py
+++ transfer_matrix.py
@@ -33,21 +33,21 @@ class Wave:
 		self.min_wl = min_wl  # Starting wavelength
 		self.max_wl = max_wl  # Ending wavelength
 		self.num_points = num_points  # Number of wavelengths between min_wl and max_wl
-		
+		self.wavelengths = []
+
 	def make_wavelengths(self):
 		"""Generate linearly-spaced wavelengths between min_wl and max_wl
 		   with number of wavelengths equal to num_points"""
-		return np.linspace(self.min_wl, self.max_wl, self.num_points)
-		
+		self.wavelengths = np.linspace(self.min_wl, self.max_wl, self.num_points)
+
 	def wavelength_to_wavenumber(self, wavelengths):
 		"""Convert micrometers to cm-1 for an array of wavelengths"""
 		return [10**4 / l for l in wavelengths]
-	
+
 	def wavenumber_to_eV(self, wavenumbers):
 		"""Convert from cm-1 to eV for an array of wavenumbers"""
 		return [sc.h * sc.c * k * 100.0 / sc.elementary_charge for k in wavenumbers]
-			
-			
+
 
 class Layer:
 
@@ -60,8 +60,8 @@ class Layer:
 		self.wavelengths = []  # array of free space wavelengths
 		self.refractive_index = []  # array of refractive indices
 		self.extinction_coeff = []  # array of extinction coefficients
-		self.waves = {}  # Needs to be curly braces
-		
+		self.complex_refractive = {}  # Needs to be curly braces
+
 	def __repr__(self):
 		a = "{} \n".format(self.material)
 		b = "thickness: {}\n".format(self.thickness)
@@ -84,8 +84,8 @@ class Layer:
 					K = float(row[2])
 					self.extinction_coeff.append(K)
 				except IndexError:
-					self.extinction_coeff.append(0.)
-				
+					self.extinction_coeff.append(0.0)
+
 	def get_data_from_txt(self, index_path):
 		"""Used for filmetrics.com data"""
 		with open(index_path, 'r') as params:
@@ -100,37 +100,41 @@ class Layer:
 				if unit:
 					wl = wl* unit
 				n = float(line[1])
-				
+
 				self.wavelengths.append(wl)
 				self.refractive_index.append(n)
 				if line[2]:
 					K = float(line[2])
 					self.extinction_coeff.append(K)
-				
-	def set_wavelengths(self):
-		"""Creates a new set of linearly-spaced wavelengths from start and 
-		   endpoint wavelengths and number of points specified in yaml config file."""
-		new_wl = np.linspace(self.min_wl, self.max_wl, num=self.num_points, endpoint=True)
-		return new_wl
 
-	def make_new_data_points(self):
-		"""Makes new data points based on user-defined num_points and interpolation."""
+# 	def set_wavelengths(self):
+# 		"""Creates a new set of linearly-spaced wavelengths from start and
+# 		   endpoint wavelengths and number of points specified in yaml config file."""
+# 		new_wl = np.linspace(self.min_wl, self.max_wl, num=self.num_points, endpoint=True)
+# 		return new_wl
+
+	def make_new_data_points(self, wavelengths):
+		"""
+		Makes new data points based on user-defined num_points and interpolation.
+		Input: list of wavelengths in micrometers generated from yaml config file.
+		Output: refractive index values (real and imaginary) mapped to wavelengths.
+		"""
+		
 		if not isinstance(self.refractive_index, list):
-			self.refractive_index = [self.refractive_index]*self.num_points	
+			self.refractive_index = [self.refractive_index]*self.num_points
 			self.extinction_coeff = [self.extinction_coeff]*self.num_points
-			self.wavelengths = self.set_wavelengths()
-			for idx, lmbda in enumerate(self.wavelengths):
-				self.waves[lmbda] = self.refractive_index[idx] + self.extinction_coeff[idx]
+			for idx, lmbda in enumerate(wavelengths):
+				self.complex_refractive[lmbda] = self.refractive_index[idx] + self.extinction_coeff[idx]
 
 		elif isinstance(self.refractive_index, list):
 			new_n = sp.interpolate.interp1d(self.wavelengths, self.refractive_index, fill_value='extrapolate')
 			new_K = sp.interpolate.interp1d(self.wavelengths, self.extinction_coeff, fill_value='extrapolate')
-			self.wavelengths = self.set_wavelengths()
-			self.refractive_index = new_n(self.wavelengths)
-			self.extinction_coeff = new_K(self.wavelengths)
-			for idx, lmbda in enumerate(self.wavelengths):
-				self.waves[lmbda] = self.refractive_index[idx] + 1j*self.extinction_coeff[idx]
-		
+			
+			self.refractive_index = new_n(wavelengths)
+			self.extinction_coeff = new_K(wavelengths)
+			for idx, lmbda in enumerate(wavelengths):
+				self.complex_refractive[lmbda] = self.refractive_index[idx] + 1j*self.extinction_coeff[idx]
+
 	def get_wavenumber(self, n, omega, theta=0):
 		"""Outputs the wavenumbers for the dielectric for the given
 		   angular frequency and angle"""
@@ -138,8 +142,6 @@ class Layer:
 		k_x = n*omega/sc.c * np.cos(theta)
 		k_z = n*omega/sc.c * np.sin(theta)
 		return k_x, k_z
-
-
 
 
 def get_dict_from_yaml(yaml_file):
@@ -155,20 +157,20 @@ def get_layers_from_yaml(device_dict):
 	val2 = 'num_points'
 	val3 = 'min_wavelength'
 	val4 = 'max_wavelength'
-	
+
 	num_layers = len(device_dict[val1])
 	num_points = int(device_dict[val2])
-	
+
 	logger.info("Num points: {}".format(num_points))
 	logger.info("Number of layers: {}".format(num_layers))
-	
+
 	# Minimum and maximum desired wavelengths
 	min_wl = float(device_dict[val3])
 	max_wl = float(device_dict[val4])
 	layers = []
-	
+
 	for i in range(num_layers):
-	
+
 		layer_str = 'layer' + str(i)
 		layer = device_dict['layers'][layer_str]
 		material = layer['material']
@@ -188,7 +190,7 @@ def get_layers_from_yaml(device_dict):
 			layer_class.wavelengths = layer['wavelength']
 		else:
 			logger.debug("ERROR: Incorrect yaml config format. Reference default template.")
-			
+
 		layers.append(layer_class)
 
 
@@ -198,16 +200,16 @@ def get_layers_from_yaml(device_dict):
 def get_beam_profile(beam_csv):
 	"""Gets field distribution data from FTIR csv file.
 	   Outputs list of wavenumbers and field amplitudes.
-	   WARNING: Jasco FTIR has weird non utf-8 characters in the footer and 
+	   WARNING: Jasco FTIR has weird non utf-8 characters in the footer and
 	   partway through data. This annoyance is kind of dealt with. Might have problems."""
-	
+
 	with open(beam_csv, 'r', encoding="utf8", errors="ignore") as beam:
 		num_header_rows = 18
 		num = 0
 		reader = csv.reader(beam)
 		wavenum = []
 		field = []
-		
+
 		while num <= num_header_rows:
 			next(reader, None)
 			num += 1
@@ -227,17 +229,17 @@ def get_beam_profile(beam_csv):
 					pass
 		except StopIteration:
 			pass
-		
+
 	return wavenum, field
 
 def attenuation_coefficient(beam_intensity, x):
-	"""NOT PROPERLY IMPLEMENTED. 
+	"""NOT PROPERLY IMPLEMENTED.
 	Takes beam intensity as array"""
-	
+
 	dI = np.gradient(beam_intensity, x)
-	
+
 	return dI / beam_intensity
-	
+
 def kramers_kronig(alpha):
 	"""NOT IMPLEMENTED.
 	Takes attenuation coefficient. Returns real part of index of refraction."""
@@ -245,32 +247,33 @@ def kramers_kronig(alpha):
 
 def propagation_matrix(wavenumber, layer_thickness):
 	"""Inputs: wave number, thickness of medium
-	   Output: propagation matrix (phase accumulation for plane wave 
+	   Output: propagation matrix (phase accumulation for plane wave
 				propagating through homogeneous medium)."""
 	phi = wavenumber*layer_thickness
 	P_i = np.array([[np.exp(-1j*phi), 0.0], [0.0, np.exp(1j*phi)]])
 
 	return P_i
 
-def dynamical_matrix(n_, theta=0.0):
+def dynamical_matrix(n_, theta=0.0, wave_type='mixed'):
 	"""Inputs: index of refraction, angle of incidence
 		Outputs: dynamical matrices for s-wave and p-wave."""
-	
+
 	# s-wave dynamical matrix
 	m = n_ * np.cos(theta)
 	Ds = np.array([[1, 1], [m, -m]])
-				 
+
 	# p-wave dynamical matrix
 	Dp = np.array([[np.cos(theta), np.cos(theta)], [n_, -n_]])
-	
-	if args.swave:
+
+	if wave_type == 's-wave':
 		return Ds
-	elif args.pwave:
+	elif wave_type == 'p-wave':
 		return Dp
-	else:
-		logger.debug("No wave polarization passed in. See --help for more info. Exiting....")
+	elif wave_type == 'mixed':
+		pol_msg = "\nNo wave acceptable polarization passed in.\nMixed-wave not yet supported.\nSee --help for more info. Exiting...."
+		logger.debug(pol_msg)
 		sys.exit()
-		
+
 
 def reflectance(M_):
     """Input: multilayer matrix, M.
@@ -283,7 +286,7 @@ def reflectance(M_):
     R = r_sq
 
     return R
-   
+
 def transmittance(TM):
     """Inputs: Multilayer matrix of dynamical matrices and propagation matrix."""
     M11 = TM.item((0, 0))
@@ -295,27 +298,27 @@ def transmittance(TM):
 
     return T
 
-def build_matrix_list(wavelength, theta, layers):
+def build_matrix_list(wavelength, theta, layers, wave_type):
 	"""
 	Makes a wavelength object and bounds. Outputs a list of matrices that will
-	make the transfer matrix for that wavelength of light propagating 
+	make the transfer matrix for that wavelength of light propagating
 	through the device."""
-	
+
 	compute_wl = wavelength * 10**(-6)
 	omega = 2 * np.pi * sc.c / compute_wl
 	matrices = []
 
 	for idx, layer in enumerate(layers):
 
-		n = layer.waves[wavelength]
+		n = layer.complex_refractive[wavelength]
 		kx = layer.get_wavenumber(n, omega, theta)[0]
-		D = dynamical_matrix(n, theta)
+		D = dynamical_matrix(n, theta, wave_type)
 		Dinv = np.linalg.inv(D)
 		P = propagation_matrix(kx, layer.thickness)
-		
+
 		if idx == 0:
 			matrices.append(Dinv)
-			
+
 		elif idx == len(layers)-1:
 			matrices.append(D)
 
@@ -323,13 +326,13 @@ def build_matrix_list(wavelength, theta, layers):
 			matrices.append(D)
 			matrices.append(P)
 			matrices.append(Dinv)
-			
+
 	return matrices
 
 
 def matrix_product(matrices):
 	"""Product of matrices"""
-	
+
 	i = 0
 	M = np.array([[1,0], [0,1]])
 	for i in matrices:
@@ -342,7 +345,7 @@ def field_amp(matrix_list, A0_, B0_):
 	Takes transfer matrix, final magnitude of EM wave, As, Bs.
 	Outputs E-field amplitude for given matrix. This approach starts calculations from
 	the incident wave."""
-	
+
 	field_rev = []  #reversed order field amplitudes
 	E0 = np.array([A0_, B0_])
 
@@ -352,7 +355,7 @@ def field_amp(matrix_list, A0_, B0_):
 	E_s = np.dot(M, E0)
 	field_rev.append(E_s)
 	matrix_list.pop(0)
-	
+
 	while i != 1:
 		M_i = matrix_product(matrix_list)
 		E_i = np.dot(M_i, E0)
@@ -366,11 +369,11 @@ def field_amp(matrix_list, A0_, B0_):
 	field = list(reversed(field_rev))
 
 	return field
-	
+
 
 def output_TRA(angle, output_dir, rows):
 	"""Writes transmission, reflectance, abosorbance data to csv file"""
-	
+
 	wavelens, trans, refl, absor = rows
 # 	output = os.path.abspath(args.output)
 	file_name = 'deg' + str(angle)
@@ -378,9 +381,9 @@ def output_TRA(angle, output_dir, rows):
 	with open (output_file, 'w') as out_file:
 	#TODO: Also output wavenumbers
 		filewriter = csv.writer(out_file, delimiter=',')
-		header = ['Wavelength', 
-				  'Transmittance', 
-				  'Reflectance', 
+		header = ['Wavelength',
+				  'Transmittance',
+				  'Reflectance',
 				  'Absorptance']
 		filewriter.writerow(header)
 		i = 0
@@ -388,10 +391,10 @@ def output_TRA(angle, output_dir, rows):
 			row = [wavelens[i], trans[i], refl[i], absor[i]]
 			filewriter.writerow(row)
 			i+=1
-	logger.info("Wrote results to {}".format(output_file))
+# 	logger.info("Wrote results to {}".format(output_file))
 	return 0
-	
-	
+
+
 def output_field_profile(wavelens, layers, E_amps):
 	"""Take list of wavelengths"""
 	k = 0.2  # test wavenumber, 2000 cm^-1
@@ -420,7 +423,7 @@ def output_field_profile(wavelens, layers, E_amps):
 			x.append(domain)
 
 	fig, ax = plt.subplots()
-	
+
 	# Generate E-field profile
 	for idx, x_i in enumerate(x):
 		f = a[idx] * np.exp(1j * k * x_i)
@@ -444,10 +447,10 @@ def output_field_profile(wavelens, layers, E_amps):
 			row = [x[i], profile[i]]
 			filewriter.writerow(row)
 			i+=1
-	logger.info("Wrote field output to {}".format(field_output))		
+	logger.info("Wrote field output to {}".format(field_output))
 
 	plt.show()
-	
+
 	return 0
 
 # ===== Fresnel equation functions below not used for now ===== #
@@ -476,7 +479,7 @@ def transmission_matrix(r_ij, t_ij):
 # ========= ========= ========= ========= ========== ========= ======== #
 
 
-def main_loop(device_yaml, output_dir):
+def main_loop(device_yaml, output_dir, wave_type):
 	"""
 	Inputs: yaml file containing information about device and incident radiation.
 	Outputs: Executes transfer matrix and other functions
@@ -492,22 +495,28 @@ def main_loop(device_yaml, output_dir):
 	num_angles = em_wave['num_angles']  # Number of angles to sweep through
 	angles = np.linspace(theta_i, theta_f, num_angles)
 	
+	# Initialize Wave class
+	
+	min_wavelength = float(device['min_wavelength'])
+	max_wavelength = float(device['max_wavelength'])
+	num_points = int(device['num_points'])
+	wave = Wave(min_wavelength, max_wavelength, num_points)
+	wave.make_wavelengths()  # still in units of um
+
 	logger.info('theta_i: {}, theta_f: {}, num angles: {}'.format(theta_i, theta_f, num_angles))
 	for layer in layers:
 		# interpolating from downloaded index data so number of data points match.
 		#TODO: Am I doing this correctly?
-		layer.make_new_data_points()
+		layer.make_new_data_points(wave.wavelengths)
 
-	#TODO: We should separate this from the Layer class.
-	wavelens = layers[0].wavelengths  # still in units of um
 
 	#TODO: Don't hard-code this directory when testing is done.
 	sim_folder = os.path.join(output_dir, 'testing_sim_folder')
 	if not os.path.exists(sim_folder):
 		os.makedirs(sim_folder)
-	
+
 	for angle in angles:
-		logger.info('Angle: {}'.format(angle))
+
 		#TODO: Organize outputs better cuz there'll probably be a lot of them.
 		# Outputs
 		T = []
@@ -515,19 +524,18 @@ def main_loop(device_yaml, output_dir):
 		A = []
 		E_amps = []
 		intensity = []
-	
+
 	# 	alpha = attenuation_coefficient(efield, k)
+		for lmbda in wave.wavelengths:
 
-		for lmbda in wavelens:
-
-			M = build_matrix_list(lmbda, angle, layers)
+			M = build_matrix_list(lmbda, angle, layers, wave_type)
 			TM = np.linalg.multi_dot(M)
 			trns = transmittance(TM).real
 			refl = reflectance(TM).real
 	# 		field = field_amp(M, device["wave"]['A0'], device['wave']['B0'])
 	# 		logging.info('Only using forward-propagating field values')
 	# 		field = [f[0] for f in field]
-			
+
 	# 		E_amps.append(field)
 			T.append(trns)
 			R.append(refl)
@@ -535,7 +543,7 @@ def main_loop(device_yaml, output_dir):
 			A.append(1 - trns - refl)
 
 		#Write everything to a csv file
-		output_TRA(angle, sim_folder, [wavelens, T, R, A])
+		output_TRA(angle, sim_folder, [wave.wavelengths, T, R, A])
 	# 	output_field_profile(wavelens, layers, E_amps)
 
 
@@ -543,7 +551,7 @@ def get_console_handler():
 	console_handler = logging.StreamHandler(sys.stdout)
 	console_handler.setFormatter(FORMATTER)
 	return console_handler
-	
+
 # def get_file_handler():
 # 	file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
 # 	file_handler.setFormatter(FORMATTER)
@@ -561,14 +569,14 @@ def get_logger(args, logger_name):
 # 	logger.addHandler(get_file_handler())
 	logger.propagate = False
 	return logger
-	
+
 def parse_arguments():
 	parser = argparse.ArgumentParser()
 	device_help = "path for a yaml file from config_files describing a device"
 	output_help = "path and name for output data file (must be .csv)"
 	pwave_help = "boolean, calculate for p-wave"
 	swave_help = "boolean, calculate for s-wave"
-	
+
 	parser.add_argument('--debug', action='store_true', help="Enable debugging.")
 	parser.add_argument("device", help=device_help)
 	parser.add_argument("output", help=output_help)
@@ -582,16 +590,26 @@ def parse_arguments():
 def main(args):
 
 	logger.debug("Debugging enabled")
-	
+
 	# NOTICE: USER NO LONGER HAS TO SPECIFY A FILE NAME
-	
+
 # 	output_message = "Output file must be .csv"
 # 	assert args.output[-4:] == ".csv", output_message
 	logger.info("Start simulation")
 	logger.info("Loading device parameters from {}".format(args.device))
 	
+	wave_type = 'mixed'
+	if args.pwave:
+		wave_type = 'p-wave'
+		logger.info("Incident wave is p-wave.")
+	elif args.swave:
+		wave_type = 's-wave'
+		logger.info("Incident wave is s-wave.")
+	else:
+		logger.info("Incident wave is mixed.")
+
 	start_time = time.time()
-	main_loop(args.device, args.output)
+	main_loop(args.device, args.output, wave_type)
 	end_time = time.time()
 	elapsed_time = np.round(end_time - start_time, 4)
 	logger.info('elapsed time: {} seconds'.format(elapsed_time))
